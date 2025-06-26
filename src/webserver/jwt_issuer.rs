41c3fe5150de5e6721978cd42dabeb0a1c16b614
Liam <dltmdgud@nate.com>
2025-06-27 00:17:35 +0900

    jwt update refresh_token

--- src/webserver/jwt_issuer.rs
+++ src/webserver/jwt_issuer.rs
@@ -83,6 +83,7 @@ pub struct ClaimsCore<T> {
 
     pub exp: i64,              // 만료 시간 (timestamp)
     pub iat: i64,              // 발급 시간 (timestamp)
+    pub nbf: i64,              // 시작 시간 (timestamp)
     pub token_type: TokenType, // access/refresh 등
 
     pub sub: u64,  // 사용자 ID
@@ -90,31 +91,25 @@ pub struct ClaimsCore<T> {
 
     // refresh 토큰은 아래 정보를 세션에 담아야 한다.
     // pub aud: u64,              // 대상자 (클라이언트 ID 등)
-    // pub nbf: usize,            // 시작 시간 (timestamp)
+
     // pub env: Environment,      // 실행환경
     // pub iss: u64,              // 발급자 (서비스 ID 등)
     // pub user_agent: String,    // 실행환경 (예: dev, prod)
     pub extra: std::marker::PhantomData<T>,
 }
 
-pub type ClaimsFromAccess = ClaimsCore<()>;
-pub type ClaimsForExchange = ClaimsCore<u64>;
-pub type ClaimsRefresh = ClaimsCore<i64>;
+pub mod detail {
+    use serde::{Deserialize, Serialize};
 
-impl From<ClaimsFromAccess> for ClaimsForExchange {
-    fn from(claims: ClaimsFromAccess) -> Self {
-        Self {
-            jti: claims.jti,
-            from: claims.from,
-            exp: claims.exp,
-            iat: claims.iat,
-            token_type: claims.token_type,
-            sub: claims.sub,
-            role: claims.role,
-            extra: std::marker::PhantomData::default(),
-        }
-    }
+    #[derive(Debug, Serialize, Deserialize, Clone)]
+    pub struct AccessTag;
+
+    #[derive(Debug, Serialize, Deserialize, Clone)]
+    pub struct RefreshTag;
 }
+
+pub type ClaimsFromAccess = ClaimsCore<detail::AccessTag>;
+pub type ClaimsRefresh = ClaimsCore<detail::RefreshTag>;
 
 impl Default for ClaimsFromAccess {
     fn default() -> Self {
@@ -125,6 +120,7 @@ impl Default for ClaimsFromAccess {
             from: Default::default(),
             iat: now,
             exp: now,
+            nbf: now,
             token_type: TokenType::default(),
             sub: Default::default(),
             role: Default::default(),
@@ -172,53 +168,6 @@ where
             jwt_manager
                 .verify_jwt(&jwt_token, None)
                 .await
-                .map_err(|e| ClaimsError::InvalidJwt(e.to_string()))
-        }
-    }
-}
-
-impl<S> FromRequestParts<S> for ClaimsForExchange
-where
-    S: Send + Sync,
-{
-    type Rejection = ClaimsError;
-
-    fn from_request_parts(
-        parts: &mut axum::http::request::Parts,
-        _state: &S,
-    ) -> impl core::future::Future<Output = Result<Self, Self::Rejection>> {
-        async move {
-            // 쿠키에서 JWT 토큰 추출
-            let cookies = parts
-                .headers
-                .get("cookie")
-                .and_then(|cookie_header| cookie_header.to_str().ok())
-                .ok_or(ClaimsError::MissingCookieHeader)?;
-
-            // 쿠키 문자열에서 JWT 토큰 찾기
-            let jwt_token = cookies
-                .split(';')
-                .find_map(|cookie| {
-                    let cookie = cookie.trim();
-                    if cookie.starts_with("access=") {
-                        Some(cookie[4..].to_string())
-                    } else {
-                        None
-                    }
-                })
-                .ok_or(ClaimsError::MissingJwtToken)?;
-
-            let jwt_manager = parts
-                .extensions
-                .get::<Arc<JwtIssuer>>()
-                .ok_or_else(|| ClaimsError::Internal(anyhow::anyhow!("JwtManager not found")))?;
-
-            let mut validation = Validation::default();
-            validation.validate_exp = false;
-            jwt_manager
-                .verify_jwt(&jwt_token, Some(validation))
-                .await
-                .map(|claims| claims.into())
                 .map_err(|e| ClaimsError::InvalidJwt(e.to_string()))
         }
     }
@@ -281,6 +230,15 @@ impl Inner {
         )
         .map(|token_data| token_data.claims)
         .map_err(anyhow::Error::from)
+        // .map_err(|er| {
+        //     er.kind().
+        //     match er.kind() {
+        //         jsonwebtoken::errors::ErrorKind::ExpiredSignature => {
+        //             anyhow::anyhow!("Token expired")
+        //         }
+        //         _ => anyhow::anyhow!("Invalid token"),
+        //     }
+        // })
     }
 
     /// 로그인 시도 (아이디/비밀번호 검증은 실제 구현 필요)
@@ -291,11 +249,13 @@ impl Inner {
         user_agent: String,
     ) -> anyhow::Result<(String, ClaimsRefresh)> {
         let now = Utc::now();
+        let timestamp = now.timestamp();
         let refresh_claims = ClaimsRefresh {
             jti: Uuid::new_v4().to_string(),
             from: user_agent,
-            iat: now.timestamp(),
+            iat: timestamp,
             exp: (now + self.refresh_ttl).timestamp(),
+            nbf: timestamp,
             token_type: TokenType::Refresh,
             sub: uid,
             role: role,
@@ -305,8 +265,9 @@ impl Inner {
         let access_claims = ClaimsFromAccess {
             jti: Uuid::new_v4().to_string(),
             from: refresh_claims.jti.clone(),
-            iat: now.timestamp(),
+            iat: timestamp,
             exp: (now + self.access_ttl).timestamp(),
+            nbf: timestamp,
             token_type: TokenType::Access,
             sub: uid,
             role: role,
@@ -317,26 +278,43 @@ impl Inner {
         Ok((access_token, refresh_claims))
     }
 
-    /// Refresh Token 검증 및 Access Token 재발급
-    /// 1. refresh_token이 JWT로서 유효한지 먼저 검증
-    /// 2. (선택) DB/Redis 등에서 추가 검증(블랙리스트, 만료, 사용자 상태 등)
-    /// 3. 검증 통과 시 새로운 access_token 발급
-    /// 실제 서비스에서는 refresh_token 재사용 방지, 블랙리스트, 만료, 사용자 상태 등 섬세한 보안 정책 필요
     pub fn refresh_access_token(
         &self,
-        access_token: &ClaimsForExchange,
         refresh_token: &ClaimsRefresh,
         user_agent: String,
     ) -> anyhow::Result<(String, ClaimsRefresh)> {
-        if access_token.exp < chrono::Utc::now().timestamp()
-            || access_token.sub != refresh_token.sub
-            || access_token.from != refresh_token.jti
-            || refresh_token.from != user_agent.to_string()
-        {
+        if refresh_token.from != user_agent {
             return Err(anyhowln!("Invalid token type"));
         }
 
-        self.login(access_token.sub, access_token.role, user_agent)
+        let now = Utc::now();
+        let timestamp = now.timestamp();
+        let refresh_claims = ClaimsRefresh {
+            jti: Uuid::new_v4().to_string(),
+            from: user_agent,
+            iat: timestamp,
+            exp: (now + self.refresh_ttl).timestamp(),
+            nbf: refresh_token.nbf,
+            token_type: TokenType::Refresh,
+            sub: refresh_token.sub,
+            role: refresh_token.role,
+            extra: Default::default(),
+        };
+
+        let access_claims = ClaimsFromAccess {
+            jti: Uuid::new_v4().to_string(),
+            from: refresh_claims.jti.clone(),
+            iat: timestamp,
+            exp: (now + self.access_ttl).timestamp(),
+            nbf: refresh_token.nbf,
+            token_type: TokenType::Access,
+            sub: refresh_token.sub,
+            role: refresh_token.role,
+            extra: Default::default(),
+        };
+
+        let access_token = self.generate_jwt(&access_claims)?;
+        Ok((access_token, refresh_claims))
     }
 }
 
@@ -392,11 +370,10 @@ impl JwtIssuer {
 
     pub async fn refresh_access_token(
         &self,
-        access_token: &ClaimsForExchange,
         refresh_token: &ClaimsRefresh,
         user_agent: String,
     ) -> anyhow::Result<(String, ClaimsRefresh)> {
         let inner = self.inner.read().await;
-        inner.refresh_access_token(access_token, refresh_token, user_agent)
+        inner.refresh_access_token(refresh_token, user_agent)
     }
 }
